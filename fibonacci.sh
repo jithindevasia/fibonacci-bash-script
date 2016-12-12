2fcabc59248074e6aafaafe846ae3c4940eec765
Jithin Devasia <jithindevasia@gmail.com>
2016-12-12 21:38:21 +0530

    Adding fibonacci script

---
+++ fibonacci.sh
@@ -0,0 +1,18 @@
+
+# ------------------------------------------
+# This script was written by Jithin Devasia
+# Email : jithindevasia@gmail.com
+# ------------------------------------------
+
+#!/bin/bash
+echo -n "What should be the fibonacci limit ? "
+read n
+first=0
+second=1
+for (( i = 0; i < n; i++ )); do
+	echo -n "$first "
+	ans=$(($first+$second))
+	second=$first
+	first=$ans
+done
+echo ""
